37abdfd59027a5c61412fb661aa863860a0b22b0
mightytry <maik.rostocki@gmail.com>
2022-04-07 17:01:36 +0200

    bug fix: connection and session management

--- plugins/calender.js
+++ plugins/calender.js
@@ -11,6 +11,7 @@ class Calender{
         this._data = null;
         this.name = "Calender";
         this.timezone = "Europe/Berlin"; // https://github.com/moment/moment-timezone/blob/develop/data/packed/latest.json
+        this.format = "ddd DD.MM.YY"; // https://momentjs.com/docs/#/parsing/string-format/
     }
 
     /**
@@ -31,15 +32,20 @@ class Calender{
      **/
     async Update(fromDay, toDay) {
         this._data = JSON.parse(await this._client.Get(`calendar/feed/plugin?${new URLSearchParams({plugin: "exam-plan", start: fromDay.format(),end: toDay.format()}).toString()}`));
-
+        
+        for (let i=0; i<this._data.length; i++){
+            this._data[i].start = time(this._data[i].start);
+            this._data[i].end = time(this._data[i].end);
+        };
     }
 
     /**
      * Formats or returns Json
      * @function
-     * @param {string} [format] format like "Exam is written on <title>"
+     * @param {String} [format] format like "Exam is written on <title>"
+     * @param {object} [ov] replacer like {title: "test"} or {title: RegExp("test")}
      **/
-    Get(format) {
+    Get(format, ov = {}) {
         if (format === undefined){
             return this._data;
         }
@@ -47,15 +53,15 @@ class Calender{
             const out = [];
             this._data.forEach(element => {
                 out.push(format.replace(new RegExp("<id>", 'g'), element.id)
-                .replace(new RegExp("<id>", 'g'), element.id)
-                .replace(new RegExp("<title>", 'g'), element.title)
-                .replace(new RegExp("<start>", 'g'), element.start)
-                .replace(new RegExp("end", 'g'), element.end)
-                .replace(new RegExp("<allDay>", 'g'), element.allDay)
-                .replace(new RegExp("<editable>", 'g'), element.editable)
-                .replace(new RegExp("<when>", 'g'), element.when)
-                .replace(new RegExp("<plugin>", 'g'), element.plugin)
-                .replace(new RegExp("<details>", 'g'), element.details));
+                .replace(new RegExp("<id>", 'g'), element.id.replace(ov.id, ""))
+                .replace(new RegExp("<title>", 'g'), element.title.replace(ov.title, ""))
+                .replace(new RegExp("<start>", 'g'), element.start.format(this.format).replace(ov.start, ""))
+                .replace(new RegExp("<end>", 'g'), element.end.format(this.format).replace(ov.end, ""))
+                .replace(new RegExp("<allDay>", 'g'), element.allDay.toString().replace(ov.allDay, ""))
+                .replace(new RegExp("<editable>", 'g'), element.editable.toString().replace(ov.editable, ""))
+                .replace(new RegExp("<when>", 'g'), element.when.replace(ov.when, ""))
+                .replace(new RegExp("<plugin>", 'g'), element.plugin.replace(ov.plugin, ""))
+                .replace(new RegExp("<details>", 'g'), element.details.toString().replace(ov.details, "")));
             });
             return out;
         }
--- tools/request.js
+++ tools/request.js
@@ -2,6 +2,7 @@ const { IServClient } = require("..")
 const { GenConnectionParams, GenReqParams, ForamtCookies, FileExists} = require("./helpers");
 const { fetchUrl } = require("fetch/lib/fetch");
 const { writeFileSync, readFileSync } = require("fs");
+const time = require("moment-timezone");
 
 class Request {
     /**
@@ -16,18 +17,87 @@ class Request {
         this.response = "";
     }
     /**
-     * Connects with the configured data
+     * Only extracts the remberme cookie for login
      * @public
-     **/
-    async Connect() {
-        if (this._client._reuseCookies && FileExists(this._client._cookieFile))
+     * @param {object} dict the dict to extract the cookie from
+     * @returns {object} the cookie
+     */
+    RememberMeCookies(dict){
+        return {"REMEMBERME": dict.REMEMBERME};
+    }
+
+    /** 
+     * Check if a cookie is expired
+     * @param {object} cookie - the cookie to check
+     * @returns {boolean} - true if expired
+    */
+    CheckCookieExpired(cookie){
+        return cookie[0]._expires < time.utc();
+    }
+
+    /**
+     * Login with saved cookies
+     * @public
+     * @returns {boolean} - true if login was successful
+    */
+    async LoginWithSavedCookies(){
+        if (this._client._reuseCookies && !this.CheckCookieExpired(this._cookies.IServSession))
         {
-            this._cookies = JSON.parse(readFileSync(this._client._cookieFile));
             this._fcookies = ForamtCookies(this._cookies);
-            await this.Send("login_check");
-            this._client.Log(this._data, this._cookies);
-            if (this._data.status == 200) return;
+            await this.Send("");
+            if (this._data.status == 200) return true;
         }
+        return false;
+    }
+    /**
+     * Login with remember me
+     * @public
+     * @returns {boolean} - true if login was successful
+     */
+    async LoginWithRememberMe(){
+        if (this._client._rememberMe && !this.CheckCookieExpired(this._cookies.REMEMBERME))
+        {
+            this._fcookies = ForamtCookies(this.RememberMeCookies(this._cookies));
+            await new Promise((resolve, reject) => {
+                fetchUrl(this._client._url, GenReqParams(this._client), async (_, data, response) => {
+                    data.status != 200 ? console.log("Error while loading: " + this._client._url) : null
+                    this._data = data;
+                    this.response = String.fromCharCode.apply(null, response);
+                    resolve(response);
+                });
+            });
+            this._cookies = this._data.cookieJar.cookies;
+            this._cookies["REMEMBERME"] = [this._cookies["REMEMBERME"][1]];
+            this._fcookies = ForamtCookies(this._cookies);
+            if (this._client._reuseCookies){
+                writeFileSync( this._client._cookieFile, JSON.stringify(this._cookies));
+            }
+            if (this._data.status == 200) return true;
+        }
+        return false;
+    }
+
+
+    /** 
+     * Check if the session is expired
+     * @public
+    */
+    async CheckSessionExpired(){
+        if (this.CheckCookieExpired(this._cookies.IServSession))
+        {
+            if (!(await this.LoginWithSavedCookies() || await this.LoginWithRememberMe()))
+            {
+                await this.Login();
+            }
+        }
+    }
+
+    /** 
+     * New login
+     * @public
+    */
+    async Login(){
+        console.log("New Login!");
         await new Promise((resolve, reject) => {
 			fetchUrl(this._client._url + "app/login", GenConnectionParams(this._client), async (_, data, response) => {
 				data.status != 200 ? console.log("Error while loading: " + this._client._url) : null
@@ -42,10 +112,37 @@ class Request {
             throw new Error("\n\nWrong login information provided!\n");
         }
         this._client.Log(this._data, this._cookies);
-        
         // for setting reuseCookies
         if (this._client._reuseCookies){
             writeFileSync( this._client._cookieFile, JSON.stringify(this._cookies));
+        }
+    }
+    /** 
+     * Parse the json expire date from cookies
+     * @private
+    */
+    ParseExpireDate(){
+        Object.keys(this._cookies).forEach(cookie => {
+            this._cookies[cookie][0]._expires = time(this._cookies[cookie][0]._expires);
+        });
+    }
+
+    /**
+     * Connects with the configured data
+     * @public
+     **/
+    async Connect() {
+        if (FileExists(this._client._cookieFile))
+        {
+            this._cookies = JSON.parse(readFileSync(this._client._cookieFile));
+            this.ParseExpireDate();
+            if (!(await this.LoginWithSavedCookies() || await this.LoginWithRememberMe()))
+            {
+                await this.Login();
+            }
+        }
+        else{
+            await this.Login();
         }
         //fetchUrl()
     }
@@ -57,6 +154,7 @@ class Request {
      **/
     async Send(url)
     {
+        await this.CheckSessionExpired();
         await new Promise((resolve, reject) => {
 			fetchUrl(this._client._url + url, GenReqParams(this._client), async (_, data, response) => {
 				data.status != 200 ? console.log("Error while loading: " + this._client._url) : null
